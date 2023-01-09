958d9cbe627a2ec69c1e5b472708e06af765c6d2
ridvan bayir | argutus gmbh <bayir@argutus.de>
2023-01-09 17:13:24 +0100

    workspace crud

--- src/models/workspace.model.ts
+++ src/models/workspace.model.ts
@@ -1,8 +1,50 @@
 import {Entity, model, property} from '@loopback/repository';
 
+interface Permission {
+  write: boolean;
+  read: boolean;
+  delete: boolean;
+}
+
+interface Members {
+  id: string;
+  permission: Permission[];
+}
+
 @model({settings: {strict: false}})
 export class Workspace extends Entity {
   // Define well-known properties here
+
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'array',
+    required: true,
+  })
+  members: Members[];
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  owner: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  description: string;
 
   // Indexer property to allow additional data
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
